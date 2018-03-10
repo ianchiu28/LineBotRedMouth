fef7e4b35d0156058a911e3b9b023d7c7065d1c7
IanChiu28 <nk9710632@gmail.com>
2018-03-10 16:18:05 +0800

    change module

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var middleware = require('@line/bot-sdk').middleware;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var webhook = require('./routes/webhook');
+//var webhook = require('./routes/webhook');
 
 var app = express();
+
+var config = {
+  channelAccessToken: process.env.CHANNEL_SECRET,
+  channelSecret: process.env.CHANNEL_ACCESS_TOKEN
+};
+
+app.post('/webhook', middleware(config), function(req, res){
+  res.json(req.body.events);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/webhook', webhook);
+//app.use('/webhook', webhook);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/webhook.js
+++ routes/webhook.js
@@ -1,24 +1,14 @@
 var express = require('express');
-var linebot = require('linebot')
+var middleware = require('@line/bot-sdk').middleware;
 var router = express.Router();
 
-var bot = linebot({
-  channelId: process.env.CHANNEL_ID,
-  channelSecret: process.env.CHANNEL_SECRET,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
-});
+var config = {
+    channelAccessToken: /*process.env.CHANNEL_SECRET*/'7931c003dd5482aa22b88e7a296b3f18',
+    channelSecret: /*process.env.CHANNEL_ACCESS_TOKEN*/'zeKP9HDE7tjaxGalU5WlnXHxyosefiW1xwxZnXP+6456Uhk6Aakz9vcTFk1TqASGx1mUqymJBbfDNKe3u7Ai5AH/QK0/97Y9k3Cwa6cgevg6XGBw3oZlTrB5/0GUUtaUS8B4N4RaX//mGncFt4tJWAdB04t89/1O/w1cDnyilFU='
+};
 
-bot.on('message', function(event) {
-  event.reply(event.message.text).then(function (data) {
-    // success
-    console.log('$$ Success $$', data);
-  }).catch(function (error) {
-    // error
-    console.log('$$ Error $$', error);
-  });
+router.post('/', middleware(config), function(req, res) {
+    res.json(req.body.events);
 });
-
-var linebotParser = bot.parser();
-router.post('/', linebotParser);
 
 module.exports = router;
