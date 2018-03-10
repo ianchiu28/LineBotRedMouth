f1ffb783a116bd0cdc4ef6110e1dfc634cae8cc4
IanChiu28 <nk9710632@gmail.com>
2018-03-10 17:17:17 +0800

    remove and mark

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require('express');
-/*var path = require('path');
+/*--var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');*/
+var bodyParser = require('body-parser');--*/
 var line = require('@line/bot-sdk');
 var middleware = require('@line/bot-sdk').middleware;
 
-/*var index = require('./routes/index');
-var users = require('./routes/users');*/
-//var webhook = require('./routes/webhook');
+/*--var index = require('./routes/index');
+var users = require('./routes/users');--*/
+//--var webhook = require('./routes/webhook');
 
 var app = express();
 
@@ -19,14 +19,13 @@ var config = {
 };
 
 // view engine setup
-/*app.set('views', path.join(__dirname, 'views'));
+/*--app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));*/
-//--app.use('/webhook', middleware(config));
-/*app.use(bodyParser.json());
+app.use(logger('dev'));--*/
+/*--app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -51,7 +50,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});*/
+});--*/
 
 app.post('/webhook', middleware(config), function(req, res){
   console.log(req.body.events);
