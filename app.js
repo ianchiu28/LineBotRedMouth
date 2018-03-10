c7c8a858b94c241dee40bb72fac56d887984b315
IanChiu28 <nk9710632@gmail.com>
2018-03-10 17:05:09 +0800

    test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');*/
+var line = require('@line/bot-sdk');
 var middleware = require('@line/bot-sdk').middleware;
 
 /*var index = require('./routes/index');
@@ -24,7 +25,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));*/
-app.use('/webhook', middleware(config));
+//--app.use('/webhook', middleware(config));
 /*app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
