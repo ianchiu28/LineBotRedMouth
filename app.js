deab8f86df70cb30c0a5a58b9df29ae2f393b2af
ianchiu28 <nk9710632@gmail.com>
2018-03-15 17:36:32 +0800

    add index

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');--*/
 
-/*--var index = require('./routes/index');
-var users = require('./routes/users');--*/
+var index = require('./routes/index');
+/*--var users = require('./routes/users');--*/
 var webhook = require('./routes/webhook');
 
 var app = express();
@@ -64,19 +64,19 @@ app.get('/db/select', (req, res) => {
 });
 
 // view engine setup
-/*--app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));--*/
-/*--app.use(bodyParser.json());
+/*--app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));*/
 
 app.use('/', index);
-app.use('/users', users);*/
+/*--app.use('/users', users);*/
 app.use('/webhook', webhook);
 
 // catch 404 and forward to error handler
