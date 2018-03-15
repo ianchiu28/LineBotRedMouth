fe3e2fa44a2ca170bb1d83da66be3424d7a6d502
ianchiu28 <nk9710632@gmail.com>
2018-03-15 17:39:56 +0800

    add public

--- app.js
+++ app.js
@@ -72,8 +72,8 @@ app.set('view engine', 'jade');
 /*--app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));*/
+app.use(cookieParser());*/
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 /*--app.use('/users', users);*/
